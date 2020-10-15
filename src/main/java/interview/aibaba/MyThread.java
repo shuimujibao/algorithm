e7f20d18e3e9b0d3ae8c55aac27894a48d5369cc
sunxiaoyu3 <sunxiaoyu3@100tal.com>
2020-10-15 16:28:35 +0800

    interview by answer

---
+++ src/main/java/interview/aibaba/MyThread.java
@@ -0,0 +1,28 @@
+package interview.aibaba;
+
+/**
+ * @author sunxiaoyu3
+ * @description 线程改名
+ * @createTime 2020/10/15 15:18:00
+ */
+public class MyThread extends Thread {
+    private MyThread() {
+
+    }
+
+
+    private MyThread(String name) {
+        super.setName(name);
+
+    }
+
+    @Override
+    public void run() {
+        System.out.println("线程-->" + currentThread().getName());
+    }
+
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread("asadasa");
+        myThread.start();
+    }
+}
