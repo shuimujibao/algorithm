d4699f49a556feaf81b7e9e094d59768012f8132
sunxiaoyu3 <sunxiaoyu3@100tal.com>
2020-10-30 16:23:32 +0800

    给定一个循环数组（最后一个元素的下一个元素是数组的第一个元素），输出每个元素的下一个更大元素。数字 x 的下一个更大的元素是按数组遍历顺序，这个数字之后的第一个比它更大的数，这意味着你应该循环地搜索它的下一个更大的数。如果不存在，则输出 -1。

--- src/main/java/netty/start/TimeClientHandler.java
+++ src/main/java/netty/start/TimeClientHandler.java
@@ -1,13 +1,9 @@
 package netty.start;
 
-import com.sun.xml.internal.ws.wsdl.writer.document.soap.Body;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelHandlerInvoker;
-import io.netty.util.concurrent.EventExecutorGroup;
 
 import java.util.logging.Logger;
 
