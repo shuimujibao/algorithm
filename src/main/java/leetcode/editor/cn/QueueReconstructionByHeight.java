7a406ae0c24452d71699e9cb04da38557cb53078
sunxiaoyu3 <sunxiaoyu3@100tal.com>
2020-11-02 16:03:00 +0800

    假设有打乱顺序的一群人站成一个队列。 每个人由一个整数对(h, k)表示，其中h是这个人的身高，k是排在这个人前面且身高大于或等于h的人数

---
+++ src/main/java/leetcode/editor/cn/QueueReconstructionByHeight.java
@@ -0,0 +1,47 @@
+package leetcode.editor.cn;
+
+//假设有打乱顺序的一群人站成一个队列。 每个人由一个整数对(h, k)表示，其中h是这个人的身高，k是排在这个人前面且身高大于或等于h的人数。 编写一个算法来
+//重建这个队列。 
+//
+// 注意： 
+//总人数少于1100人。 
+//
+// 示例 
+//
+// 
+//输入:
+//[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+//
+//输出:
+//[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+// 
+// Related Topics 贪心算法 
+// 👍 525 👎 0
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class QueueReconstructionByHeight{
+    public static void main(String[] args) {
+        Solution solution = new QueueReconstructionByHeight().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        if (people == null || people.length == 0 || people[0].length == 0) {
+            return new int[0][0];
+        }
+        Arrays.sort(people, (a, b) -> (a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]));
+        List<int[]> queue = new ArrayList<>();
+        for (int[] p : people) {
+            queue.add(p[1], p);
+        }
+        return queue.toArray(new int[queue.size()][]);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
