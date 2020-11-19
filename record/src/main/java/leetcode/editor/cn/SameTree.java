12fc887a05e6829c5d18124ffc34050430ea9eb2
sunxiaoyu3 <sunxiaoyu3@100tal.com>
2020-11-19 15:23:49 +0800

    给定两个二叉树，编写一个函数来检验它们是否相同。

---
+++ record/src/main/java/leetcode/editor/cn/SameTree.java
@@ -0,0 +1,79 @@
+package leetcode.editor.cn;
+
+//给定两个二叉树，编写一个函数来检验它们是否相同。 
+//
+// 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。 
+//
+// 示例 1: 
+//
+// 输入:       1         1
+//          / \       / \
+//         2   3     2   3
+//
+//        [1,2,3],   [1,2,3]
+//
+//输出: true 
+//
+// 示例 2: 
+//
+// 输入:      1          1
+//          /           \
+//         2             2
+//
+//        [1,2],     [1,null,2]
+//
+//输出: false
+// 
+//
+// 示例 3: 
+//
+// 输入:       1         1
+//          / \       / \
+//         2   1     1   2
+//
+//        [1,2,1],   [1,1,2]
+//
+//输出: false
+// 
+// Related Topics 树 深度优先搜索 
+// 👍 511 👎 0
+
+public class SameTree{
+    public static void main(String[] args) {
+        Solution solution = new SameTree().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode root1, TreeNode root2) {
+        // 都为空的话，显然相同
+        if (root1 == null && root2 == null) return true;
+        // 一个为空，一个非空，显然不同
+        if (root1 == null || root2 == null) return false;
+        // 两个都非空，但 val 不一样也不行
+        if (root1.val != root2.val) return false;
+
+        // root1 和 root2 该比的都比完了
+        return isSameTree(root1.left, root2.left)
+                && isSameTree(root1.right, root2.right);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
