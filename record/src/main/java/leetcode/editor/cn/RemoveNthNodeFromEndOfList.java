1d1c0e3ab25caa87e736ee51bd2b5c078cb9f293
sunxiaoyu3 <sunxiaoyu3@100tal.com>
2020-11-19 15:09:11 +0800

    给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。

--- record/src/main/java/leetcode/editor/cn/RemoveNthNodeFromEndOfList.java
+++ record/src/main/java/leetcode/editor/cn/RemoveNthNodeFromEndOfList.java
@@ -19,28 +19,48 @@ package leetcode.editor.cn;
 // Related Topics 链表 双指针 
 // 👍 1115 👎 0
 
-public class RemoveNthNodeFromEndOfList{
+public class RemoveNthNodeFromEndOfList {
     public static void main(String[] args) {
         Solution solution = new RemoveNthNodeFromEndOfList().new Solution();
-        
+
     }
 
 //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        return null;
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+
+            ListNode preNode = head;
+            ListNode curNode = head;
+
+            for (int i = 0; i < n; i++) {
+                curNode = curNode.next;
+            }
+
+            if (curNode == null) {
+                return preNode.next;
+            }
+
+            while (curNode.next != null) {
+                preNode = preNode.next;
+                curNode = curNode.next;
+            }
+
+            preNode.next = preNode.next.next;
+
+            return head;
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
