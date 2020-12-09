28d132b7caa45689b677b51eecbe56301b2b4fbc
sunxiaoyu3 <sunxiaoyu3@100tal.com>
2020-12-09 10:56:04 +0800

    给定一个二叉树，检查它是否是镜像对称的。

---
+++ record/src/main/java/leetcode/editor/cn/SymmetricTree.java
@@ -0,0 +1,82 @@
+package leetcode.editor.cn;
+
+//给定一个二叉树，检查它是否是镜像对称的。 
+//
+// 
+//
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。 
+//
+//     1
+//   / \
+//  2   2
+// / \ / \
+//3  4 4  3
+// 
+//
+// 
+//
+// 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的: 
+//
+//     1
+//   / \
+//  2   2
+//   \   \
+//   3    3
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 你可以运用递归和迭代两种方法解决这个问题吗？ 
+// Related Topics 树 深度优先搜索 广度优先搜索 
+// 👍 1151 👎 0
+
+public class SymmetricTree {
+    public static void main(String[] args) {
+        Solution solution = new SymmetricTree().new Solution();
+
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public boolean isSymmetric(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+            //调用递归函数，比较左节点，右节点
+            return dfs(root.left, root.right);
+        }
+
+        boolean dfs(TreeNode left, TreeNode right) {
+            //递归的终止条件是两个节点都为空
+            //或者两个节点中有一个为空
+            //或者两个节点的值不相等
+            if (left == null && right == null) {
+                return true;
+            }
+            if (left == null || right == null) {
+                return false;
+            }
+            if (left.val != right.val) {
+                return false;
+            }
+            //再递归的比较 左节点的左孩子 和 右节点的右孩子
+            //以及比较  左节点的右孩子 和 右节点的左孩子
+            return dfs(left.left, right.right) && dfs(left.right, right.left);
+        }
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
