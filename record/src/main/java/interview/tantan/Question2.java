7139c1551ded73b042c203da59293117be55555c
sunxiaoyu3 <sunxiaoyu3@100tal.com>
2021-06-18 18:08:39 +0800

    探探

---
+++ record/src/main/java/interview/tantan/Question2.java
@@ -0,0 +1,9 @@
+package interview.tantan;
+
+/**
+ * @author sunxiaoyu3
+ * @description 设计一个先进先出的队列，并在时间复杂度为O(1)的条件下 支持获取当前队列最大值
+ * @createTime 2021/06/18 16:22:00
+ */
+public class Question2 {
+}
