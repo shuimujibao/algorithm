c72bde31235887347a9af887efb0feff6a39e4c8
sunxiaoyu3 <sunxiaoyu3@100tal.com>
2021-07-08 20:26:29 +0800

    算法

---
+++ record/src/main/java/leetcode/dynamicprogramming/Fib.java
@@ -0,0 +1,12 @@
+package leetcode.dynamicprogramming;
+
+/**
+ * @author sunxiaoyu3
+ * @description 斐波那契额数列
+ * @createTime 2021/07/08 18:16:00
+ */
+public class Fib {
+    int fib(int N) {
+        return 0;
+    }
+}
