78a253388aa5709ec25509eba164fe220fe25e77
sunxiaoyu3 <sunxiaoyu3@100tal.com>
2021-07-14 19:07:13 +0800

    动态规划 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）

--- record/src/main/java/leetcode/editor/cn/UniquePaths.java
+++ record/src/main/java/leetcode/editor/cn/UniquePaths.java
@@ -59,7 +59,20 @@ public class UniquePaths {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int uniquePaths(int m, int n) {
-            return 0;
+            //当前网格所有位置
+            int[][] f = new int[m][n];
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i == 0 || j == 0) {
+                        f[i][j] = 1;
+                    } else {
+                        f[i][j] = f[i - 1][j] + f[i][j - 1];
+                    }
+                }
+            }
+
+            return f[m - 1][n - 1];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
