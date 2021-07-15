f4663e8cb805567fbc6eddabc696fc7f8a461a6f
sunxiaoyu3 <sunxiaoyu3@100tal.com>
2021-07-15 19:04:23 +0800

    给定一个二叉树，返回它的 后序 遍历。

---
+++ record/src/main/java/leetcode/editor/cn/BinaryTreePostorderTraversal.java
@@ -0,0 +1,64 @@
+package leetcode.editor.cn;
+
+//给定一个二叉树，返回它的 后序 遍历。 
+//
+// 示例: 
+//
+// 输入: [1,null,2,3]  
+//   1
+//    \
+//     2
+//    /
+//   3 
+//
+//输出: [3,2,1] 
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+// Related Topics 栈 树 深度优先搜索 二叉树 
+// 👍 623 👎 0
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePostorderTraversal {
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreePostorderTraversal().new Solution();
+
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public List<Integer> postorderTraversal(TreeNode root) {
+            List<Integer> list = new ArrayList<>();
+            traversal(root, list);
+            return list;
+        }
+
+        private void traversal(TreeNode root, List<Integer> list) {
+            if (root == null) {
+                return;
+            }
+            traversal(root.left, list);
+            traversal(root.right, list);
+            list.add(root.val);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
