7c0e161ed9cc2622813605faa9f2b365796e69f0
sunxiaoyu3 <sunxiaoyu3@100tal.com>
2021-07-20 17:07:03 +0800

    全排列

--- record/src/main/java/leetcode/editor/cn/Permutations.java
+++ record/src/main/java/leetcode/editor/cn/Permutations.java
@@ -1,55 +1,86 @@
 package leetcode.editor.cn;
 
-//给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。 
+//给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
 //
-// 
 //
-// 示例 1： 
 //
-// 
+// 示例 1：
+//
+//
 //输入：nums = [1,2,3]
 //输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
-// 
 //
-// 示例 2： 
 //
-// 
+// 示例 2：
+//
+//
 //输入：nums = [0,1]
 //输出：[[0,1],[1,0]]
-// 
 //
-// 示例 3： 
 //
-// 
+// 示例 3：
+//
+//
 //输入：nums = [1]
 //输出：[[1]]
-// 
 //
-// 
 //
-// 提示： 
 //
-// 
-// 1 <= nums.length <= 6 
-// -10 <= nums[i] <= 10 
-// nums 中的所有整数 互不相同 
-// 
-// Related Topics 回溯算法 
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 6
+// -10 <= nums[i] <= 10
+// nums 中的所有整数 互不相同
+//
+// Related Topics 回溯算法
 // 👍 1401 👎 0
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Permutations {
     public static void main(String[] args) {
         Solution solution = new Permutations().new Solution();
-
+        int[] nums = {1, 2, 3};
+        System.out.println(solution.permute(nums));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        List<List<Integer>> res = new LinkedList<>();
+
         public List<List<Integer>> permute(int[] nums) {
-            return null;
+            // 记录「路径」
+            LinkedList<Integer> track = new LinkedList<>();
+            backtrack(nums, track);
+            return res;
         }
+
+        // 路径：记录在 track 中
+        // 选择列表：nums 中不存在于 track 的那些元素
+        // 结束条件：nums 中的元素全都在 track 中出现
+        void backtrack(int[] nums, LinkedList<Integer> track) {
+            // 触发结束条件
+            if (track.size() == nums.length) {
+                res.add(new LinkedList(track));
+                return;
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                // 排除不合法的选择
+                if (track.contains(nums[i]))
+                    continue;
+                // 做选择
+                track.add(nums[i]);
+                // 进入下一层决策树
+                backtrack(nums, track);
+                // 取消选择
+                track.removeLast();
+            }
+        }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
